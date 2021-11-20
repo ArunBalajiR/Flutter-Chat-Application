80ee86fe609f3924a4cebac59bfa45a3dbd2013d
anan <ananalfredcarlos@gmail.com>
2021-11-20 12:54:14 +0700

    convert to null safety 2.12

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:letschat/main.dart' as app;
 
-void main() => run(_testMain);
+// void main() => run(_testMain);
 
 void _testMain() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
